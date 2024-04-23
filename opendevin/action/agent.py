d25bc8e1b82822a47e615142183f522a0681b796
Robert Brennan <contact@rbren.io>
2024-04-23 16:11:11 -0400

    beef up tests

--- opendevin/action/agent.py
+++ opendevin/action/agent.py
@@ -20,7 +20,7 @@ class AgentRecallAction(ExecutableAction):
 
     async def run(self, controller: 'AgentController') -> AgentRecallObservation:
         return AgentRecallObservation(
-            content='Recalling memories...',
+            content='',
             memories=controller.agent.search_memory(self.query),
         )
 
